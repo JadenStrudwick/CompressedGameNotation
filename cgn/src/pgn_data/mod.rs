343fced24887f5ec1005918f24897b780977c86a
Jaden Strudwick <strudjay@gmail.com>
2023-11-03 16:50:08 +0000

    100% code cov

--- cgn/src/pgn_data/mod.rs
+++ cgn/src/pgn_data/mod.rs
@@ -108,6 +108,26 @@ Qd2 Bb4 29. c3 Be7 30. Nf2 dxc3 31. bxc3 Nd8 32. Bb1 Ne6 33. Nh3 Bc5 34. Ba2 Rd8
 Bxf7+ Kf8 42. Qxf2 Rxd1 43. Bxg6 Qd6 44. g5 Qd3 45. Qc5+ Qd6 46. Qc8+ Kg7 47.
 Qxb7+ Kf8 48. Qf7# 1-0"#;
 
+    /// Example PGN string with additional header.
+    const PGN_STR_EXAMPLE_EXTRA_HEADER: &str = r#"[Event "Titled Tuesday Blitz January 03 Early 2023"]
+[Site ""]
+[Date "2023.01.03"]
+[Round "?"]
+[White "Magnus Carlsen"]
+[Black "Samvel Ter-Sahakyan"]
+[Result "1-0"]
+[Extra "FOOBAR"]
+
+1. a4 Nf6 2. d4 d5 3. Nf3 Bf5 4. Nh4 Be4 5. f3 Bg6 6. Nc3 c5 7. e4 cxd4 8. Nxg6
+hxg6 9. Qxd4 Nc6 10. Qf2 d4 11. Nd1 e5 12. Bc4 Rc8 13. Qe2 Bb4+ 14. Kf1 Na5 15.
+Bd3 O-O 16. Nf2 Qb6 17. h4 Nh5 18. Rh3 Qf6 19. g4 Nf4 20. Bxf4 Qxf4 21. h5 g5
+22. Rd1 a6 23. Kg2 Rc7 24. Rhh1 Rfc8 25. Nh3 Qf6 26. Ra1 Nc6 27. Rhc1 Bd6 28.
+Qd2 Bb4 29. c3 Be7 30. Nf2 dxc3 31. bxc3 Nd8 32. Bb1 Ne6 33. Nh3 Bc5 34. Ba2 Rd8
+35. Qe2 Nf4+ 36. Nxf4 gxf4 37. Kh3 g6 38. Rd1 Rcd7 39. Rxd7 Rxd7 40. Rd1 Bf2 41.
+Bxf7+ Kf8 42. Qxf2 Rxd1 43. Bxg6 Qd6 44. g5 Qd3 45. Qc5+ Qd6 46. Qc8+ Kg7 47.
+Qxb7+ Kf8 48. Qf7# 1-0"#;
+    
+
     #[test]
     /// Tests if the PgnData struct can be parsed and then converted back to a string.
     fn parsed_eq_original() {
@@ -129,5 +149,13 @@ Qxb7+ Kf8 48. Qf7# 1-0"#;
         assert_eq!(pgn_data.white, "");
         assert_eq!(pgn_data.black, "");
         assert_eq!(pgn_data.result, "");
+    }
+
+    #[test]
+    /// Tests if additional headers are ignored when parsing a PGN string.
+    fn ignores_additional_headers() {
+        let pgn_str = PGN_STR_EXAMPLE_EXTRA_HEADER;
+        let pgn_data = super::PgnData::from_str(pgn_str);
+        assert!(pgn_data.to_string().find("FOOBAR").is_none());
     }
 }
