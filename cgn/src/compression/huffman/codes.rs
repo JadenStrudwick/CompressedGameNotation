56c940c31c01218f042be452e29c07e04a5bc63c
Jaden Strudwick <strudjay@gmail.com>
2023-11-21 19:44:31 +0000

    Renamed huffman_codes

--- cgn/src/compression/huffman/codes.rs
+++
@@ -1,7 +0,0 @@
-// use std::collections::HashMap;
-
-// use huffman_compress::{Book, CodeBuilder, Tree};
-
-// pub fn lichess_huffman_weights() -> (Book<u8>, Tree<u8>) {
-//     let mut weights: HashMap<u8, u32> = HashMap::new();
-// }
