75750d2c2c5d859a7ce334acb5736a8ad63e1f42
Jaden Strudwick <strudjay@gmail.com>
2024-01-17 18:22:17 +0000

    Browser extension POC

--- cgn-extension/index.js
+++ cgn-extension/index.js
@@ -3,38 +3,126 @@ import init, {
   dynamic_huffman_compress_pgn_str,
 } from './cgn.js';
 
+// Function to convert a Uint8Array to a hexadecimal string
 function toHexString(byteArray) {
-  return Array.from(byteArray, function (byte) {
-    return ('0' + (byte & 0xff).toString(16)).slice(-2);
-  }).join('');
+  return Array.from(byteArray, (byte) =>
+    ('0' + (byte & 0xff).toString(16)).slice(-2)
+  ).join('');
 }
 
-function hexStringToIntArray(hexString) {
-  let intArray = [];
-  for (let i = 0; i < hexString.length; i += 2) {
-    intArray.push(parseInt(hexString.substr(i, 2), 16));
+// Function to convert a hexadecimal string to a Uint8Array
+function toUint8Array(hexString) {
+  const length = hexString.length;
+  const uint8Array = new Uint8Array(length / 2);
+  for (let i = 0; i < length; i += 2) {
+    uint8Array[i / 2] = parseInt(hexString.substring(i, i + 2), 16);
   }
-  return intArray;
+  return uint8Array;
 }
+
+// Get elements from DOM
+const compressInput = document.getElementById('compress-input');
+const compressCopyButton = document.getElementById('compress-copy-button');
+const compressDownloadButton = document.getElementById(
+  'compress-download-button'
+);
+const decompressInput = document.getElementById('decompress-input');
+const decompressCopyButton = document.getElementById('decompress-copy-button');
+const decompressDownloadButton = document.getElementById(
+  'decompress-download-button'
+);
+
+// Handle drag and drop
+compressInput.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+decompressInput.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+// Drop event handler
+compressInput.addEventListener('drop', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const file = e.dataTransfer.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    compressInput.value = reader.result;
+  };
+  reader.readAsText(file);
+});
+decompressInput.addEventListener('drop', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const file = e.dataTransfer.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    decompressInput.value = toHexString(new Uint8Array(reader.result));
+  };
+  reader.readAsArrayBuffer(file);
+});
+
+// Handle mouse leave
+compressCopyButton.addEventListener('mouseleave', () => {
+  compressCopyButton.innerText = 'Copy (Hexadecimal)';
+});
+compressDownloadButton.addEventListener('mouseleave', () => {
+  compressDownloadButton.innerText = 'Download Compressed File';
+});
+decompressCopyButton.addEventListener('mouseleave', () => {
+  decompressCopyButton.innerText = 'Copy (PGN Content)';
+});
+decompressDownloadButton.addEventListener('mouseleave', () => {
+  decompressDownloadButton.innerText = 'Download PGN File';
+});
 
 async function run() {
   await init();
 
-  document.getElementById('compress-input').addEventListener('input', () => {
-    let text = document.getElementById('compress-input').value;
-    let compressed = dynamic_huffman_compress_pgn_str(text);
-    let hex_string = toHexString(compressed);
-    document.getElementById('compress-output').innerHTML = hex_string;
+  // handle compress copy button
+  compressCopyButton.addEventListener('click', () => {
+    const compressed = dynamic_huffman_compress_pgn_str(compressInput.value);
+    navigator.clipboard.writeText(toHexString(compressed));
+    compressCopyButton.innerText = 'Copied!';
   });
 
-  document.getElementById('decompress-input').addEventListener('input', () => {
-    let text = document.getElementById('decompress-input').value;
-    let intArray = hexStringToIntArray(text);
-    let decompressed = dynamic_huffman_decompress_pgn_str(intArray);
-    document.getElementById('decompress-output').innerHTML = decompressed;
-    console.log(text, intArray, decompressed);
+  // handle compress download button
+  compressDownloadButton.addEventListener('click', () => {
+    const compressed = dynamic_huffman_compress_pgn_str(compressInput.value);
+    const blob = new Blob([compressed], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'compressed.cgn';
+    a.click();
+    URL.revokeObjectURL(url);
+    compressDownloadButton.innerText = 'Downloaded!';
   });
 
-  console.log('Done');
+  // handle decompress copy button
+  decompressCopyButton.addEventListener('click', () => {
+    const decompressed = dynamic_huffman_decompress_pgn_str(
+      toUint8Array(decompressInput.value)
+    );
+    navigator.clipboard.writeText(decompressed);
+    decompressCopyButton.innerText = 'Copied!';
+  });
+
+  // handle decompress download button
+  decompressDownloadButton.addEventListener('click', () => {
+    const decompressed = dynamic_huffman_decompress_pgn_str(
+      toUint8Array(decompressInput.value)
+    );
+    const blob = new Blob([decompressed], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'decompressed.pgn';
+    a.click();
+    URL.revokeObjectURL(url);
+    decompressDownloadButton.innerText = 'Downloaded!';
+  });
 }
 run();
